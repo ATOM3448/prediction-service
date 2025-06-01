126f5665138435256ba8a81309888613380bdd43
atom3448 <qwery2003@outlook.com>
2025-06-02 03:12:59 +0700

    Подготовлена почва для реализации бизнес-логики

--- service/src/main/java/ru/tusur/prediction/service/api/prediction/controller/StudentController.java
+++ service/src/main/java/ru/tusur/prediction/service/api/prediction/controller/StudentController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.tusur.prediction.service.api.prediction.ApiPaths;
 import ru.tusur.prediction.service.api.prediction.dto.result.StudentPredictionResult;
+import ru.tusur.prediction.service.core.ml.MlService;
 
 /**
  * Контроллер для работы с запросами предсказаний по студентам.
@@ -12,6 +13,8 @@ import ru.tusur.prediction.service.api.prediction.dto.result.StudentPredictionRe
 @RestController(ApiPaths.PREDICTION_API_STUDENT)
 @AllArgsConstructor
 public class StudentController {
+
+    private final MlService mlService;
 
     @GetMapping
     public StudentPredictionResult getPredictionForStudent() {
